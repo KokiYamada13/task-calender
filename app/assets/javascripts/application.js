4fb68a749d34d31f86df8fa95071453fcadfbd07
Yamada Koki <lenny.srv.826@gmail.com>
2020-06-16 23:55:37 +0900

    Add:  カレンダーにイベント表示機能追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,6 +18,52 @@
 //= require moment
 //= require fullcalendar
 
-$(() => {
-    $('#calendar').fullCalendar({});
-})
+$(function () {
+    $(document).on('turbolinks:load', function () {
+        if ($('#calendar').length) {
+
+            function Calendar() {
+                return $('#calendar').fullCalendar({
+                });
+            }
+            function clearCalendar() {
+                $('#calendar').html('');
+            }
+
+            $(document).on('turbolinks:load', function () {
+                Calendar();
+            });
+            $(document).on('turbolinks:before-cache', clearCalendar);
+
+            $('#calendar').fullCalendar({
+                events: '/schedules.json',
+                titleFormat: 'YYYY年 M月',
+                dayNamesShort: ['日', '月', '火', '水', '木', '金', '土'],
+                header: {
+                    left: '',
+                    center: 'title',
+                    right: 'today prev,next'
+                },
+                defaultTimedEventDuration: '03:00:00',
+                buttonText: {
+                    prev: '前',
+                    next: '次',
+                    prevYear: '前年',
+                    nextYear: '翌年',
+                    today: '今日',
+                    month: '月',
+                    week: '週',
+                    day: '日'
+                },
+                editable: true,
+                timeFormat: "HH:mm",
+                eventColor: '#87cefa',
+                eventTextColor: '#000000',
+                eventRender: function(event, element) {
+                    element.css("font-size", "0.8em");
+                    element.css("padding", "5px");
+                }
+            });
+        }
+    });
+});
